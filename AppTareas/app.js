0b955bdd891c4488298991e892e45ef8d7e72aea
emac5F <emac109508@gmail.com>
2025-03-11 20:09:37 -0400

    Implement task addition, deletion, and completion features with event listeners in JavaScript; update task template structure in HTML and add SVG assets for task actions.

--- AppTareas/app.js
+++ AppTareas/app.js
@@ -1,7 +1,43 @@
 const taskContainer = document.querySelector('.task__container');
 const taskForm = document.querySelector('.task__form');
 const taskTemplate = document.querySelector(".task__template");
-const taskContainerButtons = document.querySelector('.task__buttons');
+const taskContainerButtons = document.querySelector(".task__buttons");
+
+// Evento para agregar una tarea
+taskForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const inputFormValue = taskForm.querySelector(".task__input").value;
+    const task = createTask(inputFormValue);
+    renderTask(task);
+    taskForm.reset();
+});
+
+// Evento para eliminar una tarea
+taskContainer.addEventListener('click', function(e) {
+    const currentElement = e.target;
+    // Eliminar tarea
+    if(currentElement.matches('.task__delete, .task__delete *')) {
+        const currentTask = currentElement.closest('[data-task]');
+        currentTask.remove();
+    }
+    // Marcar tarea como completada
+    else if(currentElement.matches(".task__status")) {
+        const currentTask = currentElement.closest("[data-task]");
+        const span = currentTask.querySelector(".task__name");
+        span.contenteditable = !currentElement.checked;
+    }
+});
+
+// navegar entre tareas
+taskContainerButtons.addEventListener("click", function(e) {
+    const currentElement = e.target;
+    if(currentElement.matches(".task__btn")) {
+        const currentButtonActive = taskContainerButtons.querySelector(".task__btn--active");
+        currentButtonActive.classList.remove("task__btn--active");
+
+        currentElement.classList.add("task__btn--active");
+    }
+});
 
 //funcion que crea la tarea
 function createTask(name) {
@@ -15,8 +51,16 @@ function createTask(name) {
 
 // funcion que renderiza las tareas en el dom
 function renderTask(task) {
-    const currentTaskElement = taskTemplate.textContent.cloneNode(true);
-    currentTaskElement.querySelector('[data-task]').setAttribute('data-id', task.id);
-    currentTaskElement.querySelector('.task__name').textContent = task.name;
-    currentTaskElement.querySelector('.task__status').checked = task.done;
+    const currentTaskElement = taskTemplate.content.cloneNode(true);
+
+    const p = currentTaskElement.querySelector('[data-task]');
+    p.setAttribute("data-id", task.id);
+
+    const span =  currentTaskElement.querySelector(".task__name");
+    span.textContent = task.name;
+
+    const inputCheckbox = currentTaskElement.querySelector(".task__status");
+    inputCheckbox.checked = task.done;
+
+    taskContainer.append(currentTaskElement);
 }
