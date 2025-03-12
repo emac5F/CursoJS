725df8b5ef90bf72558fc841ef56d9c8ccfe66d4
emac5F <emac109508@gmail.com>
2025-03-11 22:33:55 -0400

    Enhance navigation with submenu functionality and transitions; add arrow SVG for visual cues

--- AppTareas/navegacion.js
+++ AppTareas/navegacion.js
@@ -1,12 +1,29 @@
 const buttonMenu = document.querySelector('.button');
 const buttonImg = document.querySelector('.button__img');
 
+const nav = document.querySelector('.nav');
+
+const list = document.querySelector('.list');
+
 buttonMenu.addEventListener('click', function() {
     if(buttonMenu.getAttribute("data-info") === "open") {
         buttonImg.src = "./assets/close.svg";
         buttonMenu.setAttribute("data-info", "close");
+    
+        nav.classList.add('nav--show');
+
     } else{
         buttonImg.src = "./assets/menu.svg";
         buttonMenu.setAttribute("data-info", "open");
+
+        nav.classList.remove('nav--show');
     }
 });
+
+list.addEventListener('click', function(e){
+    const currentElement = e.target;
+    if(currentElement.matches('.element-p')) {
+       const listElement = currentElement.closest(".element");
+       listElement.classList.toggle("element-show");
+    }
+});   
